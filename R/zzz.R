f0a3440d8ffc13eb8c860d3bd47558828cfc84f4
Philipp Franikowski <philipp.franikowski@gmail.com>
2025-03-28 08:46:10 +0100

    fix start-up message

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,7 @@
 .onAttach <- function(libname, pkgname) {
-  cli::cli_alert_info("{.pkg {pkgname}} v{packageVersion(pkgname)}")
+  version <- utils::packageVersion(pkgname)
+
+  cli::cli_alert_info("{.pkg {pkgname}} v{version}",
+                      class = "packageStartupMessage")
   # rlang::inform(message = msg, class = "packageStartupMessage")
 }
-
